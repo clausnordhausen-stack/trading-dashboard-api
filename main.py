3341b3386a28d83cf102fafa6b3770391a68a2a9
Claus Nordhausen <claus.nordhausen@gmail.com>
2026-03-30 14:52:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-# main.py
-
 import uvicorn
 
 if __name__ == "__main__":
     uvicorn.run(
-        "app:app",   # wichtig: app.py -> app Objekt
+        "app:app",
         host="0.0.0.0",
         port=10000,
-        reload=True
+        reload=True,
     )
